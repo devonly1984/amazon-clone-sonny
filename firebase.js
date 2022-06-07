0c6bbb108aa8b296f4ae5c4d607f606d49979f27
Bill White <devonly1984@hotmail.com>
2022-06-07 10:50:42 -0500

    Adding nextauth,googleauth and firebase

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCigMF4bWT_og3qG80qB-YuHByAjq77zrc",
+    authDomain: "clone-352615.firebaseapp.com",
+    projectId: "amazon-clone-352615",
+    storageBucket: "amazon-clone-352615.appspot.com",
+    messagingSenderId: "780339145376",
+    appId: "1:780339145376:web:96ba77bba4c62f87cef7c5"
+  };
+  
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,23 @@
-import Image from "next/image";
 import {
-  SearchIcon,
   MenuIcon,
+  SearchIcon,
   ShoppingCartIcon,
 } from "@heroicons/react/outline";
+
+import Image from "next/image";
+
 const Header = () => {
   return (
     <header>
       <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
         <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
-          <img
-            src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
-            width="150"
-            height="40"
-            className="cursor-pointer object-contain"
+          <Image
+            src="https://links.papareact.com/f90"
+            width={150}
+            height={40
+            }
+            objectFit="contain"
+            className="cursor-pointer "
           />
         </div>
         {/**Search */}
