f48febfac55c1c3764621ffb98edce7be4d51215
John NoBody <devonly1984@hotmail.com>
2022-06-11 05:06:07 -0500

    Starting stripe intergration

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -5,10 +5,27 @@ import Image from "next/image";
 import { selectItems, selectTotal } from "../slices/basketSlice";
 import { useSelector } from "react-redux";
 import { useSession } from "next-auth/react";
+import { loadStripe } from "@stripe/stripe-js";
+import axios from "axios";
+const stripePromise = loadStripe(process.env.stripe_public_key);
 const Checkout = () => {
   const items = useSelector(selectItems);
   const { data: session } = useSession();
   const total = useSelector(selectTotal);
+  const createCheckoutSession = async () => {
+    const stripe = await stripePromise;
+    const checkoutSession = await axios.post("/api/create-checkout-session", {
+      items,
+      email: session?.user?.email,
+    });
+    const result = await stripe.redirectToCheckout({
+      sessionId: checkoutSession.data.id,
+    });
+
+    if (result.error) {
+      alert(result.error.message);
+    }
+  };
   return (
     <div className="bg-gray-100">
       <Header />
@@ -44,6 +61,8 @@ const Checkout = () => {
                   </span>
                 </h2>
                 <button
+                  role="link"
+                  onClick={createCheckoutSession}
                   disabled={!session}
                   className={`button mt-2 ${
                     !session &&
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,8 @@ import Header from "../components/Header";
 import ProductFeed from "../components/ProductFeed";
 
 export default function Home({ products }) {
+  //
+
   return (
     <div className="bg-gray-100">
       <Head>
@@ -13,7 +15,7 @@ export default function Home({ products }) {
       <Header />
       <main className="max-w-screen-2xl mx-auto">
         <Banner />
-       
+
         <ProductFeed products={products} />
       </main>
     </div>
@@ -23,6 +25,7 @@ export const getServerSideProps = async (ctx) => {
   const products = await fetch("https://fakestoreapi.com/products")
     .then((res) => res.json())
     .catch((err) => console.log(err.message));
+
   return {
     props: {
       products,
